30d708ab278b740444455fb9dcc60c1c9f8c14f9
Ilia <fortyhell@gmail.com>
2020-11-27 20:38:05 +0300

    Add adding device on client

--- web-client/src/components/App/App.tsx
+++ web-client/src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Map from "../map/Map";
+import Device from '../Device/Device'
 import "./App.css";
 
 const App: React.FC = () => {
   return (
+	<>
       <Map/>
+      <Device/>
+	</>
   );
 };
 
