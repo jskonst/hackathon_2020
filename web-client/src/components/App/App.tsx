534858eef6c6a9a145b8b805501710f436d75639
QinamruG <tyukalov.ko3700@gmail.com>
2020-11-27 16:34:28 +0300

    upd dependencies + refactor

--- web-client/src/components/App/App.tsx
+++ web-client/src/components/App/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
+import Map from "../map/Map";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <Map center={[56.99, 40.97]} zoom={14}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </Map>
+      <Map/>
   );
 };
 
---
+++ web-client/src/components/map/Map.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+// import { ApiMapPoints } from "../../api/ApiMapPoints";
+import "../app/App.css";
+
+const MapPlaceholder: React.FC = () => {
+  return (
+    <p>
+      Map of London.{" "}
+      <noscript>You need to enable JavaScript to see this map.</noscript>
+    </p>
+  );
+};
+
+let position = { lat: 56.99, lng: 40.97 };
+const zoom = 14;
+
+const Map: React.FC = () => {
+  // const [coords, coodrsmass] = useState({ lat: 56.99, lng: 40.97 });
+
+  // useEffect(() => {
+  //   const points = ApiMapPoints();
+  //   coodrsmass.setState(points);
+  // },[]);
+  return (
+    <MapContainer center={position} zoom={zoom} placeholder={MapPlaceholder}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      {/* <Polyline positions ={positions} />  */}
+    </MapContainer>
+  );
+};
+export default Map;
