e73638be64f687db5d369e0f5714e03362fa199a
QinamruG <tyukalov.ko3700@gmail.com>
2020-11-28 06:15:13 +0300

    fixes

--- web-client/src/components/map/Map.tsx
+++ web-client/src/components/map/Map.tsx
@@ -23,24 +23,14 @@ const MapPlaceholder: React.FC = () => {
 const startPosition = { lat: 56.99, lng: 40.97 };
 const zoom = 14;
 
-const fakePositions = [
-  { lat: 56.23, lng: 40.217 },
-  { lat: 56.35, lng: 40.357 },
-  { lat: 56.42, lng: 40.467 },
-  { lat: 56.56, lng: 40.527 },
-  { lat: 56.67, lng: 40.697 },
-  { lat: 56.79, lng: 40.757 },
-  { lat: 56.99, lng: 40.97 },
-];
-
 const Map: React.FC = () => {
   const [positions, setPositions] = useState<ICoordinate[]>([]);
 
   useEffect(() => {
     const getPos = async () => {
-      let data: IServerResponse[] | undefined = await ApiGetPositions();
+      const data: IServerResponse[] | undefined = await ApiGetPositions();
       if (data !== undefined) {
-        let result: ICoordinate[] = data.map((item) => {
+        const result: ICoordinate[] = data.map((item) => {
           return { lat: item.latitude, lng: item.longitude };
         });
         setPositions(result);
